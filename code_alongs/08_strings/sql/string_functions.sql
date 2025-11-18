5e29ecb718d7c214add43ea801f7c1f645ee9e73
Haza Jijan <hazajijan@gmail.com>
2025-11-18 20:37:15 +0100

    done 08_strings

---
+++ code_alongs/08_strings/sql/string_functions.sql
@@ -0,0 +1,55 @@
+-- remove leading and trailing spaces 
+SELECT
+    trim(sql_word, ' ') as trimmed_word,
+    trimmed_word[1] as frist_character, 
+    trimmed_word[-1] as last_character, 
+FROM
+    staging.sql_glossary; 
+
+
+-- transform character to uppercase 
+SELECT
+    upper(trim(sql_word, ' ')) as sql_term,
+    sql_term[1] as frist_character, 
+    sql_term[-1] as last_character, 
+FROM
+    staging.sql_glossary; 
+
+-- replace 2 more spaces with 1 space 
+SELECT
+    description, 
+    replace(description, '  ', ' ') as cleaned_description
+    FROM staging.sql_glossary; 
+
+
+-- concatenate string 
+SELECT
+    concat(upper(trim(sql_word, ' ')) 'command') as sql.command,
+
+FROM
+    staging.sql_glossary; 
+
+-- extract substrings
+SELECT 
+	trim(sql_word), as trimmed_word,
+	substring(trim(sql_word), 1, 5) as first_fice_chars, 
+    trimmed_word[1:5] as sliced_five_chars,
+
+FROM staging.sql_glossary
+
+-- reverse characters
+SELECT 
+	reverse(trim(sql_word)) as reversed_word
+FROM staging.sql_glossary
+
+-- find the position of the first occurence of a substring
+-- return zero if the substring is not found
+
+SELECT 
+	description,
+	instr(description, 'select') as select_position,
+    select_position != 0 as about_select
+FROM staging.sql_glossary;
+    
+-- concatenation
+SELECT 'fun' || ' joke'
