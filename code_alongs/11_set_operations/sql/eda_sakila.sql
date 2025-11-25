7c9493c06d33dd78a862ffffb27908042ab503f6
Haza Jijan <hazajijan@gmail.com>
2025-11-25 14:30:14 +0100

    done sakila

---
+++ code_alongs/11_set_operations/sql/eda_sakila.sql
@@ -0,0 +1,33 @@
+DESC;
+
+-- 1000 films
+FROM film;
+
+DESC TABLE film;
+
+SELECT DISTINCT rating FROM film;
+
+-- 5462 > 1000 films -> actors can play ≥ 1 film
+FROM film_actor;
+
+-- ex insight
+-- actor_id 1 -> Penelope Guiness
+-- plays in movies: academy dinosaur, anaconda confessions, ...
+FROM actor;
+
+-- bridge table between film and category
+-- it holds film_id and category_id
+FROM film_category;
+FROM category;
+
+SELECT 
+    'customer' AS type,
+    c.first_name || ' ' || c.last_name AS name
+FROM customer c; -- alias 
+
+
+SELECT 
+    'customer' AS type,
+    c.first_name || ' ' || c.last_name AS name
+FROM customer c
+WHERE c.first_name ILIKE 'b%';
